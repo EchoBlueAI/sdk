9f6e5078b37deb84c102a640ebc1f997aa608f76
Kenan Stipek <kenan@stipek.org>
2024-03-18 15:00:26 -0700

    add bearer token support

--- templates/redux/src/data/empty.api.ts
+++ templates/redux/src/data/empty.api.ts
@@ -1,6 +1,15 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'https' }),
+// get token from local storage
+export const getTokenFromLocalStorage = () => {
+  return localStorage.getItem("x-authorization-echoblue");
+};
+
+// initialize an empty api service that we'll inject endpoints into later as needed
+export const emptySplitApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3001/",
+    headers: { Authorization: `Bearer ${getTokenFromLocalStorage()}` },
+  }),
   endpoints: () => ({}),
-})
+});
